b884f821ad49ba6d7a1db8a9705ccce9e00e2dc7
Luke-Moyer <luke@moyeraudio.com>
2021-02-10 08:00:30 -0600

    Rearranged crawl.py def order

--- crawl.py
+++ crawl.py
@@ -7,6 +7,45 @@ import json
 from colors import *
 
 
+def call_files(dir="./test_files") -> None:
+    """
+    Compiles each file in directory. Sends to `crawl()` for parsing.
+    Dependencies [
+    os,
+    scantree (local),
+    crawl (local)
+    ]
+
+    :param dir: str of relative directory to search
+    """
+    for files in scantree(dir):
+        rel_file = os.path.relpath(files.path, dir)
+        local_path = "{}/{}".format(dir,rel_file)
+
+        crawl(local_path)
+
+
+def scantree(path:str) -> dict:
+    """Recursively yield DirEntry objects for given directory.
+    Dependencies: [
+    os,
+    exclude_path(local),
+    ]
+    :param path: str local directory to search
+    :return: obj with data about the files, namely: file path
+
+    """
+    for entry in os.scandir(path):
+        if exclude_path(entry.path) == False:
+            continue
+        if entry.is_dir(follow_symlinks=False):
+            yield from scantree(entry.path)
+        else:
+            if include_file_type(entry.path):
+                continue
+            yield entry
+
+
 def crawl(haystack:str) -> None:
     """Creates an index for every significant word used in supplied file. Scores the word by how many times it is used plus if it occurs in title of html.
     Dependencies{
@@ -82,6 +121,28 @@ def crawl(haystack:str) -> None:
     print("completed")
 
 
+def exclude_path(file:str) -> False:
+    type = "exclude_path.txt"
+
+    """return False if supplied path matches any line in exclusion file"""
+    exclude_path = list(open(type).read().splitlines())
+    for ex in exclude_path:
+        if ex in file: ## must be in to match part of path rather than all of it
+            return False
+    return True
+
+
+def exclude_word(file:str) -> False:
+    type = "exclude_words.txt"
+
+    """return False if supplied word matches any word in exclusion file"""
+    exclude_path = list(open(type).read().splitlines())
+    for ex in exclude_path:
+        if ex == file: ##must be == to match whole word rather than part of it
+            return False
+    return True
+
+
 def count_words(needle:str, haystack:list) -> int:
     """Counts how many times str needle is exact match to haystack list items"""
     man_count = 0
@@ -102,65 +163,13 @@ def include_file_type(file:str) -> False:
     return True
 
 
-def exclude_word(file:str) -> False:
-    type = "exclude_words.txt"
-
-    """return False if supplied word matches any word in exclusion file"""
-    exclude_path = list(open(type).read().splitlines())
-    for ex in exclude_path:
-        if ex == file: ##must be == to match whole word rather than part of it
-            return False
-    return True
 
 
-def exclude_path(file:str) -> False:
-    type = "exclude_path.txt"
-
-    """return False if supplied path matches any line in exclusion file"""
-    exclude_path = list(open(type).read().splitlines())
-    for ex in exclude_path:
-        if ex in file: ## must be in to match part of path rather than all of it
-            return False
-    return True
 
 
-def scantree(path:str) -> dict:
-    """Recursively yield DirEntry objects for given directory.
-    Dependencies: [
-    os,
-    exclude_path(local),
-    ]
-    :param path: str local directory to search
-    :return: obj with data about the files, namely: file path
-
-    """
-    for entry in os.scandir(path):
-        if exclude_path(entry.path) == False:
-            continue
-        if entry.is_dir(follow_symlinks=False):
-            yield from scantree(entry.path)
-        else:
-            if include_file_type(entry.path):
-                continue
-            yield entry
 
 
-def call_files(dir="./test_files") -> None:
-    """
-    Compiles each file in directory. Sends to `crawl()` for parsing.
-    Dependencies [
-    os,
-    scantree (local),
-    crawl (local)
-    ]
 
-    :param dir: str of relative directory to search
-    """
-    for files in scantree(dir):
-        rel_file = os.path.relpath(files.path, dir)
-        local_path = "{}/{}".format(dir,rel_file)
-
-        crawl(local_path)
 
 
 call_files('./test_files/demo')
