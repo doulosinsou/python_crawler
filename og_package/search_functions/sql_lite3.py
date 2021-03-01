da9c8017dbb51883043da4934567b241639a2202
Luke-Moyer <luke@moyeraudio.com>
2021-03-01 15:23:03 -0600

    Successful custom sqlite3 class

--- og_package/search_functions/sql_lite3.py
+++ og_package/search_functions/sql_lite3.py
@@ -1,32 +1,64 @@
 import sqlite3
 import json
 
-connection = sqlite3.connect('index/test.db')
-cursor = connection.cursor()
+class sql_ite:
+    """Establishes connection and executes calls to local database"""
+    def __init__(self):
+        self.connection = sqlite3.connect('index/crawled.db')
+        self.c = self.connection.cursor()
+        name = "crawled"
+        cols = "path text, title text, mod integer, list blob"
+        self.makeTable(name, cols)
 
-createT = """
-CREATE TABLE scrape
-(word text, path text, title text, count integer, in_title integer, list blob)
-"""
-try:
-    cursor.execute(createT)
-except sqlite3.OperationalError as err:
-    print(err)
+    def doit(self, thing):
+        try:
+            self.c.execute(thing)
+        except sqlite3.OperationalError as err:
+            print(err)
+
+    def makeTable(self, name, cols):
+        createT = "CREATE TABLE {0}({1})".format(name, cols)
+        self.doit(createT)
+
+    def addRow(self, table, content):
+        createR = "INSERT INTO {0} VALUES{1}".format(table, content)
+        self.doit(createR)
+
+    def makeFletter(self, fletter):
+        cols = "id integer, word text, count integer, in_title integer"
+        self.makeTable(fletter, cols)
+
+    def id(self):
+        find = "SELECT last_insert_rowid() from crawled"
+        self.doit(find)
+        return self.c.fetchone()
+
+    def commit(self):
+        self.connection.commit()
+
+    def close(self):
+        self.connection.close()
 
 
-addR = """
-INSERT INTO scrape VALUES(?,?,?,?,?,?)
-"""
+
+slite = sql_ite()
+
+fletter = "a"
 blob = json.dumps(['this','is','a','list'])
-addV = ('aakbar','this_path/index/','alderan',1,1,blob,)
+newcrawled = ('path/goes/here','my path',20210301,blob)
 
-cursor.execute(addR, addV)
-connection.commit()
+slite.makeFletter(fletter)
+slite.addRow('crawled', newcrawled)
 
+id = slite.id()[0]
+newRowData = (id, 'aakbar', 5, 0)
+slite.addRow(fletter, newRowData)
 
-cursor.execute("SELECT * FROM scrape WHERE word=?",('aakbar',))
-stuff = cursor.fetchall()
+slite.commit()
+slite.c.execute("SELECT * FROM a WHERE word=?",('aakbar',))
+stuff = slite.c.fetchall()
 for s in stuff:
     print(s)
 
-connection.close()
+
+slite.close()
